3b15d0d60688551d44b7f1dcc2cd803824f2f100
chaocai <chaocai@192.168.7.71>
2018-04-27 17:48:05 +0800

    The example codes/configurations

---
+++ main/g_met_examples.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+
+	. "github.com/chaocai2001/g_met"
+)
+
+//The following example is to demonstrate how to use GMet.
+func main() {
+	//create a metric writer
+	writer, err := CreateMetWriterBySeeLog("../configs/g_met_seelog/g_met_log.xml")
+	if err != nil {
+		panic(err)
+	}
+	//create GMet instance by given the writer and the formatter
+	gmet := CreateGMetInstance(writer, &LtrFormatter{})
+	//Create a metric item of host IP, the name of the metric item is "HostAddr"
+	addr, _ := IpAddress()
+	for i := 0; i < 100; i++ {
+		gmet.Send(addr, Metric("input_bytes", strconv.Itoa(rand.Intn(100))),
+			Metric("output_bytes", strconv.Itoa(rand.Intn(100))))
+		gmet.Flush() //in your real case, DON'T flush for each sending.
+		//For seelog writer, the auto-flushing can be set in the log configuration
+		time.Sleep(time.Second * 1)
+
+	}
+}
