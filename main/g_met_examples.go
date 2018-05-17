e709a973f5ea109d3cb9959e397f65e966a0907b
chaocai <chaocai@192.168.7.71>
2018-05-17 22:31:05 +0800

    changed the example codes according to the new features

--- main/g_met_examples.go
+++ main/g_met_examples.go
@@ -9,7 +9,7 @@ import (
 
 //The following example is to demonstrate how to use GMet.
 func main() {
-	//Create GMet Instance with default settings (SeelogWriter and LtrFormatter)
+	//Create GMet Instance with default settings (SeelogWriter and JSON_Formatter)
 	gmet := CreateGMetInstanceByDefault("../configs/g_met_seelog/g_met_log.xml")
 
 	//To set the writer and formatter, you can refer to the following code
@@ -18,17 +18,13 @@ func main() {
 	//		panic(err)
 	//	}
 	//	//create GMet instance by given the writer and the formatter
-	//	gmet := CreateGMetInstance(writer, &LtrFormatter{})
-
-	//Create a metric item of host IP, the name of the metric item is "HostAddr"
+	//	gmet := CreateGMetInstance(writer, &JSON_Formatter{})
 
 	for i := 0; i < 100; i++ {
 		gmet.Send(Metric("input_bytes", rand.Intn(100)),
 			Metric("output_bytes", rand.Intn(100)))
-
 		gmet.Flush() //in your real case, DON'T flush for each sending.
 		//For seelog writer, the auto-flushing can be set in the log configuration
 		time.Sleep(time.Second * 1)
-
 	}
 }
